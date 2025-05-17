6e0fa8af860b7dd028b2a46651ccaf1038462e8a
Mateus-Kazu <mateuskrj@gmail.com>
2025-05-17 14:27:19 -0300

    jogo.py

--- projetos-pessoais/jogo-em-python/jogo.py
+++ projetos-pessoais/jogo-em-python/jogo.py
@@ -1,1 +1,16 @@
-# Aqui você pode desenvolver seu próprio jogo em Python (ex: adivinhação)
+import random
+
+numero_secreto = random.randint(1, 10)
+tentativas = 0
+print("Adivinhe o número entre 1 e 10")
+
+while True:
+    chute = int(input("Seu palpite: "))
+    tentativas += 1
+    if chute == numero_secreto:
+        print(f"Acertou em {tentativas} tentativa(s)!")
+        break
+    elif chute < numero_secreto:
+        print("Muito baixo. Tente novamente.")
+    else:
+        print("Muito alto. Tente novamente.")
